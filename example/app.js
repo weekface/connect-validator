01b6c5548a301698b933ab2b8e4fc90148eb3dc6
WeekFace <weekface@gmail.com>
2010-12-10 18:14:10 +0800

    fix bug

--- example/app.js
+++ example/app.js
@@ -2,12 +2,13 @@ var express = require('express');
 var app = express.createServer();
 
 var Validate = require('../lib/connect-validator').Validate;
-
+var validate = new Validate();
 //validate()的调用会返回一个中间件,
 //这个时候需要向vadalite()传递一个我们需要验证的字段的对象
-app.get('/book',Validate({
+app.get('/book',validate.core({
     name: function(){
-        this.isNull();
+        this.isIP();
+        console.log("isNull..........");
     }
 }), function(req,res){
     if(req.errmsg){
